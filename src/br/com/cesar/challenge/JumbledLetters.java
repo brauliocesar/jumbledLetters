7aa4139b2e8523b9f49a5d9afefc33a0ae0d658e
Braulio Neto <bnetogil@gmail.com>
2020-08-01 13:34:32 -0400

    change loggic comparation

--- src/br/com/cesar/challenge/JumbledLetters.java
+++ src/br/com/cesar/challenge/JumbledLetters.java
@@ -1,8 +1,80 @@
 package br.com.cesar.challenge;
 
+/**
+ * Class to verify jumbled letters string
+ */
 public class JumbledLetters {
 
 
+    private static final int STR_MIN_LEN = 3;
 
+    /**
+     * Verify Jumbled Letters In Strings
+     *
+     * @param strOne
+     * @param strTwo
+     * @return
+     */
+    public boolean verify(final String strOne, final String strTwo) throws Exception {
+        if (null == strOne || null == strTwo ) {
+            throw new Exception("Error: String One and Two cannot be null!");
+        }
 
+        int iStrOneLength = strOne.length();
+        int iStrTwoLength = strTwo.length();
+
+        if (iStrOneLength < STR_MIN_LEN
+                || iStrTwoLength < STR_MIN_LEN) {
+            throw new Exception("Error: String One and Two has more than 3 letters!");
+        }
+
+        boolean bResult = false;
+        char[] szStringOne = strOne.toCharArray();
+        char[] szStringTwo = strTwo.toCharArray();
+
+        // Verify when strings with 3 letters only
+        if (STR_MIN_LEN == iStrOneLength
+                && STR_MIN_LEN == iStrTwoLength) {
+            bResult = szStringOne[0] == szStringTwo[0]
+                    && szStringOne[1] == szStringTwo[2];
+        } else {
+            bResult = szStringOne[0] == szStringTwo[0]
+                    && szStringOne[iStrOneLength - 1] == szStringTwo[iStrTwoLength - 1]
+                    && verifyShuffledLetters(szStringOne, iStrOneLength, szStringTwo, iStrTwoLength);
+
+        }
+
+        return bResult;
+    }
+
+    private boolean verifyShuffledLetters(final char[] strShuffledOne, int iStrOneLength,
+                                          final char[] strShuffledTwo, int iStrTwoLength) {
+        int iCount = 0;
+        boolean bResult = true;
+        for (int iIndexArray = 1; iIndexArray < iStrOneLength - 1;) {
+
+            if ((strShuffledOne[iIndexArray] == strShuffledTwo[iIndexArray + 1]
+                && strShuffledOne[iIndexArray + 1] == strShuffledTwo[iIndexArray])
+                || strShuffledOne[iIndexArray] == strShuffledTwo[iIndexArray]) {
+                iCount = 0;
+                if (strShuffledOne[iIndexArray] == strShuffledTwo[iIndexArray]) {
+                    iIndexArray++;
+                    continue;
+                }
+                iIndexArray += 2;
+                if (strShuffledOne[iIndexArray] == strShuffledTwo[iIndexArray]) {
+                    iIndexArray++;
+                    continue;
+                }
+            } else {
+                iCount++;
+            }
+
+            if (iCount == 2){
+                bResult = false;
+                break;
+            }
+        }
+        return bResult;
+    }
 }
--- src/br/com/cesar/challenge/Main.java
+++ src/br/com/cesar/challenge/Main.java
@@ -3,6 +3,23 @@ package br.com.cesar.challenge;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("JumbledLetters");
+        final JumbledLetters jumbledLetters = new JumbledLetters();
+
+        try {
+            System.out.println("you, yuo -> " + jumbledLetters.verify("you", "yuo"));
+            System.out.println("can, cna -> " + jumbledLetters.verify("can", "cna"));
+            System.out.println("read, raed -> " + jumbledLetters.verify("read", "raed"));
+            System.out.println("probably, porbalby -> " + jumbledLetters.verify("probably", "porbalby"));
+            System.out.println("this, tihs -> " + jumbledLetters.verify("this", "tihs"));
+            System.out.println("easily, esaliy -> " + jumbledLetters.verify("easily", "esaliy"));
+            System.out.println("despite, desptie -> " + jumbledLetters.verify("despite", "desptie"));
+            System.out.println("the, teh -> " + jumbledLetters.verify("the", "teh"));
+            System.out.println("misspellings, msispelilgns -> " + jumbledLetters.verify("misspellings", "msispelilgns"));
+            System.out.println("moon, nmoo -> " + jumbledLetters.verify("moon", "nmoo"));
+            System.out.println("misspellings, mpeissngslli -> " + jumbledLetters.verify("misspellings", "mpeissngslli"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
